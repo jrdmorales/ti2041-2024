281992a7f33ff43ee674afd14ef42c990754bdbb
jrdmorales <jmichael.morales23@gmail.com>
2024-10-14 20:22:50 -0300

    update models
    Se realizan cambios en los modelos

--- Evaluaciones/Sumativa 1/gestion_productos/productos/models.py
+++ Evaluaciones/Sumativa 1/gestion_productos/productos/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Marca(models.Model):
+    nombre = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nombre
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nombre
+
+class Caracteristica(models.Model):
+    nombre = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nombre
+class Producto(models.Model):
+    codigo = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    caracteristicas = models.CharField(Caracteristica,max_length=50)

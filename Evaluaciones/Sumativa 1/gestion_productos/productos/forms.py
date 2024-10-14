8c2682213d05651e241ad747d60fc1bdab400175
jrdmorales <jmichael.morales23@gmail.com>
2024-10-14 20:21:58 -0300

    update form
    Se cambia el modelo del form

--- Evaluaciones/Sumativa 1/gestion_productos/productos/forms.py
+++ Evaluaciones/Sumativa 1/gestion_productos/productos/forms.py
@@ -1,7 +1,7 @@
 from django import forms
+from .models import Producto
 
-class ProductoForm(forms.Form):
-    codigo = forms.CharField(max_length=100)
-    nombre = forms.CharField(max_length=100)
-    marca = forms.CharField(max_length=100)
-    fecha_vencimiento = forms.DateField(widget=forms.SelectDateWidget)
+class ProductoForm(forms.ModelForm):
+    class Meta:
+        model = Producto
+        fields = ['codigo', 'nombre', 'precio', 'marca', 'categoria', 'caracteristicas']

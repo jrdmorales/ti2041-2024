794d4079de23c2de7a30ffe68d5c38e83839ec1d
Jordan Morales <jmichael.morales23@gmail.com>
2024-10-21 00:22:00 -0300

    update urls

--- Evaluaciones/Sumativa2/gestion_productos/gestion_productos/urls.py
+++ Evaluaciones/Sumativa2/gestion_productos/gestion_productos/urls.py
@@ -8,7 +8,10 @@ urlpatterns = [
     path('registro/', views.registro_producto, name='registro_producto'),
     path('resultado/', views.resultado_producto, name='resultado_producto'),
     path('consulta/', views.consulta_productos, name='consulta_productos'),
+    path('filtrar/', views.filtrar_productos, name='filtrar_productos'),
+    path('listar/', views.listar_productos, name='listar_productos'),
     path('productos/', views.inicio, name='inicio'),
     path('', lambda request: redirect('productos/')),
+
 
 ]

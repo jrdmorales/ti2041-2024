de8be112c571c98ac141a66b070f70f2c5a0109d
Jordan Morales <jmichael.morales23@gmail.com>
2024-10-21 00:22:48 -0300

    modific models

--- Evaluaciones/Sumativa2/gestion_productos/productos/models.py
+++ Evaluaciones/Sumativa2/gestion_productos/productos/models.py
@@ -11,12 +11,16 @@ class Categoria(models.Model):
 
 class Caracteristica(models.Model):
     nombre = models.CharField(max_length=50)
+    
     def __str__(self):
         return self.nombre
+    
+
 class Producto(models.Model):
-    codigo = models.CharField(max_length=20)
+    codigo = models.CharField(max_length=50)
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    caracteristicas = models.CharField(Caracteristica,max_length=50)
+    caracteristicas = models.ManyToManyField(Caracteristica)
+

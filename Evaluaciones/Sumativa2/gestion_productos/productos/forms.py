ec63be403e7b1812e6f89711b478f97de50875eb
Jordan Morales <jmichael.morales23@gmail.com>
2024-10-21 00:22:16 -0300

    add clean precio en forms

--- Evaluaciones/Sumativa2/gestion_productos/productos/forms.py
+++ Evaluaciones/Sumativa2/gestion_productos/productos/forms.py
@@ -5,3 +5,9 @@ class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
         fields = ['codigo', 'nombre', 'precio', 'marca', 'categoria', 'caracteristicas']
+
+    def clean_precio(self):
+        precio = self.cleaned_data.get('precio')
+        if precio < 0:
+            raise forms.ValidationError('El precio no puede ser negativo')
+        return precio
